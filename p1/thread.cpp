2f7872c781e3ab617b99e628c751abed73c32181
stan <feihu_guest@qq.com>
2016-07-05 11:11:08 +0800

    - Added thread.cpp p1

---
+++ p1/thread.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <thread>
+
+void thread_task() {
+    std::cout << "hello thread" << std::endl;
+}
+
+
+int main(int argc, const char *argv[]) {
+    std::thread t(thread_task);
+    t.join();
+
+    return EXIT_SUCCESS;
+}
