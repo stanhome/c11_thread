fe562a4dfb21ca21a17f27bc61d4dbdd9c6f57a8
stan <feihu_guest@qq.com>
2016-07-05 11:24:44 +0800

    - changed gitignore file to ignore Clion files. - Changed the p1 log file

--- p1/thread.cpp
+++ p1/thread.cpp
@@ -5,13 +5,16 @@
 #include <thread>
 
 void thread_task() {
-    std::cout << "hello thread" << std::endl;
+    std::cout << "[thread_task] hello thread" << std::endl;
 }
 
 
 int main(int argc, const char *argv[]) {
+    std::cout << "main begin" << std::endl;
     std::thread t(thread_task);
+    std::cout << "after t instance" << std::endl;
     t.join();
+    std::cout << "after t join" << std::endl;
 
     return EXIT_SUCCESS;
 }
